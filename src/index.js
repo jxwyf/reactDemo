111c109972e27992804df82948b31b6c3c6c9a20
LWQ <2578831517@qq.com>
2021-07-18 11:50:03 +0800

    列表页面制作以及路由配置

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import reactDom from 'react-dom';
 import { Layout } from 'antd';
 import 'antd/dist/antd.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import AppHead from './components/Header/index';
 
 import './style.css';
+import List from './containers/List';
+import Detail from './containers/Detail';
 
 const { Header, Footer, Content } = Layout;
 
@@ -15,7 +18,14 @@ class App extends Component {
         return (
             <Layout style={{ minWidth: 1300 }}>
                 <Header className="header"><AppHead/></Header>
-                <Content className="content">Content</Content>
+                <Content className="content">
+                    <BrowserRouter>
+                    <Switch>
+                    <Route path='/detail' component={Detail} />
+                    <Route path='/' component={List} />  
+                    </Switch>
+                    </BrowserRouter>
+                </Content>
                 <Footer className="footer">Footer</Footer>
             </Layout>
         )
